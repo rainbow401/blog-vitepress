04bb8049523579419a539f61e7c67bb623f41533
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2023-01-04 14:35:56 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io 修改仓库地址
-git push -f git@github.com:yanzhihao1019/yanzhihao1019.github.io.git master:gh-pages
+git push -f git@github.com:yanzhihao1019/blog-vitepress.git master:gh-pages
 
 cd -
