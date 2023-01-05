5af5b7b3b0e12eeb40abf2759d5c8a2c8a30d7da
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2023-01-05 08:53:30 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-    head: [
-      ['link', { rel: 'icon', href: '/favicon.ico' }]
+    head: [ // 增加自定义标签
+      ['link', { rel: 'icon', href: '/favicon.ico' }] // 标签图标
     ],
-    base: '/blog-vitepress/',
-    lastUpdated: true,
-    title: '自定义的 title',
-    outDir: '../dist',
-    description: '自定义的 description',
-    themeConfig: {
+    base: '/blog-vitepress/', // 仓库名称
+    lastUpdated: true, // 开启更新时间
+    title: '自定义的 title', // 左上角标题
+    outDir: '../dist', // 编译路径
+    description: '自定义的 description', // 网站描述
+    themeConfig: { // 主题配置
         nav: [
           { text: 'Guide', link: '/guide/what-is-vitepress', activeMatch: '/guide/what-is-vitepress' },
           {
@@ -21,7 +21,7 @@ export default defineConfig({
           }
         ],
         socialLinks: [{ icon: "github", link: "https://github.com" }],
-        sidebar: {
+        sidebar: { // 导航，路由匹配
             '/guide/': [
               {
                 text: 'Guide',
@@ -33,17 +33,17 @@ export default defineConfig({
               }
             ]
         },
-        docFooter: { prev: '上一篇', next: '下一篇' },
+        docFooter: { prev: '上一篇', next: '下一篇' }, // 文章页脚
         footer: { // 网站页脚
             message: 'Released under the MIT License.',
             copyright: 'Copyright © 2019-present Evan You'
         },
-        editLink: {
+        editLink: { // 编辑跳转
             pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
             text: 'Edit this page on GitHub'
         },
         lastUpdatedText: "最近更新时间", // 文章更新时间
-        logo: '/icon.jpg'
+        logo: '/icon.jpg' // 左上角图标
     },
     // locales: { // 国际化
     //     "/": {
